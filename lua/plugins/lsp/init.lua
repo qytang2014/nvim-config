221416d44fd088c532161a958ed9f42f0c038a87
Tom <qytang2014@126.com>
2023-02-07 17:17:45 +0800

    lsp config限定offsetEncoding

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -85,6 +85,7 @@ return {
 
             local servers = opts.servers
             local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
+            capabilities.offsetEncoding = 'utf-8'
 
             local function setup(server)
                 local server_opts = servers[server] or {}
